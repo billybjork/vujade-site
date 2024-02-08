72b4826bafc10d1b01444c0591ad9f1199e589de
billybjork <billybjork@live.com>
2024-02-07 20:22:53 -0800

    Initial commit

--- api/api.py
+++ api/api.py
@@ -1,8 +1,104 @@
-from flask import Flask
-import time
+import os
+from flask import Flask, send_from_directory, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
-app = Flask(__name__)
+# Define the path to the React build directory
+react_build_directory = '/Users/billy/Dropbox (Personal)/VU JA DE/Other/Site Revamp/vujade-site/build'
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+# Initialize the Flask app with the React build directory as the place to serve static files from
+app = Flask(__name__, static_folder=react_build_directory, static_url_path='')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://utbipcpkawqzmn:24ecb9ddf008f0f50f7b61b5906e44a83e6b711f80a00075235cdc734c1120ee@ec2-44-213-151-75.compute-1.amazonaws.com:5432/dbimjuf7evlrhe'
+db = SQLAlchemy(app)
+
+# Configure CORS
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+# Model definitions
+class Video(db.Model):
+    __tablename__ = 'videos'
+    videoid = db.Column(db.Text(), primary_key=True)  # Updated column name
+    videoname = db.Column(db.Text(), nullable=False)  # Updated column name
+    url = db.Column(db.Text(), nullable=False)  # Updated column name
+    description = db.Column(db.Text(), nullable=True)  # Updated column name
+    sources = db.Column(db.Text(), nullable=True)  # Updated column name
+    scenes = db.relationship('Scene', backref='video', lazy=True)
+
+class Scene(db.Model):
+    __tablename__ = 'scenes'
+    sceneid = db.Column(db.Integer, primary_key=True)  # Updated column name
+    videoid = db.Column(db.Text(), db.ForeignKey('videos.videoid'), nullable=False)  # Updated column name
+    sceneurl = db.Column(db.Text(), nullable=False)  # Updated column name
+
+# Serve the React application's index.html file for the root path and any other undefined paths
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(react_build_directory, path)):
+        return send_from_directory(react_build_directory, path)
+    else:
+        return send_from_directory(react_build_directory, 'index.html')
+
+# API route to fetch videos
+@app.route('/videos')
+def get_videos():
+    try:
+        videos = Video.query.all()
+        return jsonify([{ 'videoID': video.videoid, 'videoName': video.videoname, 'URL': video.url, 'Description': video.description, 'Sources': video.sources, 'Scenes': [{'sceneURL': scene.sceneurl} for scene in video.scenes] } for video in videos])
+    except Exception as e:
+        print(f"Error in get_videos: {e}")
+        return jsonify({"error": str(e)}), 500
+
+# API route to fetch all scenes
+@app.route('/scenes')
+def get_all_scenes():
+    """Endpoint to fetch all scenes."""
+    try:
+        scenes = Scene.query.all()
+        return jsonify([{ 'sceneURL': scene.sceneurl, 'videoID': scene.videoid } for scene in scenes])
+    except Exception as e:
+        print(f"Error in get_all_scenes: {e}")
+        return jsonify({"error": str(e)}), 500
+
+# API route to fetch scenes for a specific videoID
+@app.route('/scenes/<string:videoID>')
+def get_scenes(videoID):
+    """Endpoint to fetch scenes for a specific videoID."""
+    try:
+        scenes = Scene.query.filter_by(videoid=videoID).all()
+        return jsonify([{ 'sceneURL': scene.sceneurl, 'videoID': scene.videoid } for scene in scenes]) if scenes else jsonify({"message": "No scenes found for this videoID"}), 404
+    except Exception as e:
+        print(f"Error in get_scenes: {e}")
+        return jsonify({"error": str(e)}), 500
+
+# API route to fetch video information by videoID
+@app.route('/video_info/<string:videoID>')
+def get_video_info(videoID):
+    try:
+        # Fetch the video by videoID
+        video_info = Video.query.filter_by(videoid=videoID).first()
+        if video_info:
+            # Construct the video information, including associated scenes
+            video_data = {
+                'videoID': video_info.videoid,
+                'videoName': video_info.videoname,
+                'URL': video_info.url,
+                'Description': video_info.description,
+                'Sources': video_info.sources,
+                'Scenes': [{'sceneID': scene.sceneid, 'sceneURL': scene.sceneurl} for scene in video_info.scenes]
+            }
+            return jsonify(video_data)
+        else:
+            return jsonify({"message": "Video not found"}), 404
+    except Exception as e:
+        print(f"Error in get_video_info: {e}")
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,189 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import './App.css';
+import { useInView } from 'react-intersection-observer';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import dotenv from 'dotenv';
+dotenv.config();
 
-function App() {
+// Ensure BASE_URL is defined using the environment variable or fallback to a default
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:5000';
+
+function Video({ src, videoID, onVideoClick }) {
+  const videoRef = useRef(null);
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: '50px 0px',
+  });
+
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      const playPromise = videoRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.catch(error => {
+          // Handle interrupted play request here
+          console.error("Play was interrupted.", error);
+        });
+      }
+    }
+  };  
+
+  const handleMouseLeave = () => {
+    if (videoRef.current && !videoRef.current.paused) {
+      videoRef.current.pause();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={ref} style={{ width: '100%', height: 'auto' }}>
+      {inView && (
+        <video
+          ref={videoRef}
+          src={src}
+          loop
+          muted
+          playsInline
+          onClick={() => onVideoClick(videoID)}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          style={{ width: '100%', height: 'auto' }}
+        />
+      )}
     </div>
   );
 }
 
-export default App;
+function Modal({ videoInfo, onClose }) {
+  const getEmbeddedVideoUrl = (url) => {
+    const vimeoId = url.split("vimeo.com/")[1].split('/')[0];
+    return `https://player.vimeo.com/video/${vimeoId}`;
+  };
+
+  const handleModalContentClick = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div className="modal" onClick={onClose}>
+      <div className="modal-content" onClick={handleModalContentClick}>
+        <span className="close" onClick={onClose}>&times;</span>
+        <h2>{videoInfo.videoName}</h2>
+        <div className="embed-container">
+          <iframe
+            src={getEmbeddedVideoUrl(videoInfo.URL)}
+            frameBorder="0"
+            allow="autoplay; fullscreen"
+            allowFullScreen
+            title={videoInfo.videoName}
+          ></iframe>
+        </div>
+        <p style={{ whiteSpace: 'pre-wrap' }}>{videoInfo.Description || ''}</p>
+      </div>
+    </div>
+  );
+}
+
+function App({ scenes, uniqueVideoIDs, selectedVideoInfo, setSelectedVideoInfo }) {
+  const navigate = useNavigate();
+
+  const handleVideoNameClick = (videoID) => {
+    setSelectedVideoInfo(null); // Clear any previous video info
+    // Fetch detailed video information using the correct relative endpoint
+    axios.get(`${BASE_URL}/video_info/${videoID}`)
+      .then(response => {
+        setSelectedVideoInfo(response.data);
+        navigate(`/videos/${videoID}`);
+      })
+      .catch(error => {
+        console.error('Error fetching video info: ', error);
+      });
+  };
+
+  const handleCloseModal = () => {
+    setSelectedVideoInfo(null);
+    navigate('/');
+  };
+
+  return (
+    <div className="App">
+      <div className="video-menu">
+        {uniqueVideoIDs.map(({ videoID, videoName }) => (
+          <div key={videoID} onClick={() => handleVideoNameClick(videoID)}>
+            {videoName}
+          </div>
+        ))}
+      </div>
+      <div className="video-grid">
+        {scenes.map(scene => (
+          <Video
+            key={scene.sceneURL}
+            src={scene.sceneURL}
+            videoID={scene.videoID}
+            onVideoClick={() => handleVideoNameClick(scene.videoID)}
+          />
+        ))}
+      </div>
+      {selectedVideoInfo && <Modal videoInfo={selectedVideoInfo} onClose={handleCloseModal} />}
+    </div>
+  );
+}
+
+function AppWrapper() {
+  const [scenes, setScenes] = useState([]);
+  const [selectedVideoInfo, setSelectedVideoInfo] = useState(null);
+  const [uniqueVideoIDs, setUniqueVideoIDs] = useState([]);
+
+  useEffect(() => {
+    // Fetch scenes and unique video IDs
+    axios.get(`${BASE_URL}/scenes`)
+      .then(response => {
+        setScenes(shuffleArray(response.data));
+      })
+      .catch(error => {
+        console.error('Error fetching scenes: ', error);
+      });
+
+    axios.get(`${BASE_URL}/videos`)
+      .then(response => {
+        setUniqueVideoIDs(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching videos: ', error);
+      });
+  }, []);
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/videos/:videoID"
+          element={<App
+            scenes={scenes}
+            uniqueVideoIDs={uniqueVideoIDs}
+            selectedVideoInfo={selectedVideoInfo}
+            setSelectedVideoInfo={setSelectedVideoInfo}
+          />}
+        />
+        <Route
+          path="/"
+          element={<App
+            scenes={scenes}
+            uniqueVideoIDs={uniqueVideoIDs}
+            selectedVideoInfo={selectedVideoInfo}
+            setSelectedVideoInfo={setSelectedVideoInfo}
+          />}
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppWrapper;
