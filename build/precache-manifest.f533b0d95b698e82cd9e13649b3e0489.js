b67f7e831cae9ffd534badc79133b068fb85c538
billybjork <billybjork@live.com>
2024-02-08 21:15:59 -0800

    Removed /build from .gitignore

---
+++ build/precache-manifest.f533b0d95b698e82cd9e13649b3e0489.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9dd8a70ab7a44f5a9d8c4a9dd8576cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba269ba1926067916ddb",
+    "url": "/static/css/main.579e0575.chunk.css"
+  },
+  {
+    "revision": "2ca9a245e80079852961",
+    "url": "/static/js/2.267b2b8e.chunk.js"
+  },
+  {
+    "revision": "c02595be373d5f75f3b0b9d93909c59c",
+    "url": "/static/js/2.267b2b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abdf913ff98dc4e0fa7d",
+    "url": "/static/js/3.dbd2182a.chunk.js"
+  },
+  {
+    "revision": "ba269ba1926067916ddb",
+    "url": "/static/js/main.41cd70b2.chunk.js"
+  },
+  {
+    "revision": "bf05a918918b9268a2e3",
+    "url": "/static/js/runtime-main.4ce955ce.js"
+  }
+]);
